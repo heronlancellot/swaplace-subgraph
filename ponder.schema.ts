60974c5d206f20bc29749d193018a72652aec731
Rafael de Souza Sanches <rafaeldss12@gmail.com>
2024-03-26 17:46:46 -0300

    fix: enum updated with capslock and corrected type of allowed

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,17 +1,17 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  Status: p.createEnum(["created", "accepted", "canceled"]),
+  Status: p.createEnum(["CREATED", "ACCEPTED", "CANCELED"]),
   Database: p.createTable({
     id: p.hex(),
+    blockTimestamp: p.bigint(),
+    transactionHash: p.hex(),
     status: p.enum("Status"),
-    allowed: p.hex(),
+    swapId: p.bigint(),
     owner: p.hex(),
+    allowed: p.hex(),
     expiry: p.bigint(),
     bid: p.string(),
     ask: p.string(),
-    swapId: p.bigint(),
-    blockTimestamp: p.bigint(),
-    transactionHash: p.hex(),
   }),
 }));
