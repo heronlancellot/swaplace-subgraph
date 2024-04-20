ee9efffcaa73c96de9bd9456440a230b341448ca
0xneves <asguilift@hotmail.com>
2024-04-20 07:06:06 +0700

    feat: kakarot config

--- ponder.config.ts
+++ ponder.config.ts
@@ -9,13 +9,24 @@ export default createConfig({
       chainId: 11155111,
       transport: http(process.env.ALCHEMY_RPC_URL),
     },
+    kakarot: {
+      chainId: 1802203764,
+      transport: http(process.env.KAKAROT_RPC_URL),
+    },
   },
   contracts: {
     Swaplace: {
       abi: SwaplaceAbi,
-      address: "0xFA682bcE8b1dff8D948aAE9f0fBade82D28E1842",
-      network: "sepolia",
-      startBlock: 5719042,
+      network: {
+        sepolia: {
+          address: "0xFA682bcE8b1dff8D948aAE9f0fBade82D28E1842",
+          startBlock: 5719042,
+        },
+        // kakarot: {
+        //   address: "0x11FA19B071faB6E119dBefFfaa23eFb79F14f4A2",
+        //   startBlock: 3394,
+        // },
+      },
     },
   },
 });
