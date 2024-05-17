91848672cf306583880ab87d2a0fd1d7f33f9c03
Rafael Sanches <rafaeldss12@gmail.com>
2024-05-17 12:14:37 -0300

    fix: commented `kakarot` network to prevent bugs

--- ponder.config.ts
+++ ponder.config.ts
@@ -22,10 +22,10 @@ export default createConfig({
           address: "0xFA682bcE8b1dff8D948aAE9f0fBade82D28E1842",
           startBlock: 5719042,
         },
-        kakarot: {
-          address: "0xB317127b50b22e62637E3c333A585a8ccfd0721D",
-          startBlock: 90,
-        },
+        // kakarot: {
+        //   address: "0xB317127b50b22e62637E3c333A585a8ccfd0721D",
+        //   startBlock: 90,
+        // },
       },
     },
   },
